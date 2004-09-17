0a46b4872b339c303b7b1f2df92a5922ef672711
ian <ian@5035da28-9fce-0310-abbb-aec50abfc0e8>
2004-09-17 21:18:01 +0000

    - started work on the Reduce pathway; got the setup page done.
    git-svn-id: file:///home/smlewis/svn/git-mp3/another_svn/tmp/tmp/trunk/molprobity3@77 5035da28-9fce-0310-abbb-aec50abfc0e8

--- config/config.php
+++ config/config.php
@@ -80,6 +80,6 @@ define("MP_EMAIL_AUTHOR", "moler@kinemage.biochem.duke.edu,iwd@duke.edu");
 define("MP_SESSION_NAME", "MolProbSID");
 
 // Current "internal reference" version number. Please DO NOT change.
-define("MP_VERSION", "3beta7");
+define("MP_VERSION", "3beta8");
 
 ?>
