6b3d793f1d640d81b1225d494344644926edbcd1
iwd <iwd@5035da28-9fce-0310-abbb-aec50abfc0e8>
2004-11-01 16:09:34 +0000

    - eliminated symlink-to-binary setup for other executables, except gawk on OS X - removed unneeded statements from makelinks.sh
    git-svn-id: file:///home/smlewis/svn/git-mp3/another_svn/tmp/tmp/trunk@93 5035da28-9fce-0310-abbb-aec50abfc0e8

--- molprobity3/makelinks.sh
+++ molprobity3/makelinks.sh
@@ -15,17 +15,12 @@ chmod 777 tmp/
 
 # Create symlinks for executables
 cd bin
-ln -s pdbcns.010504.pl pdbcns
+# Nothing to do here...
 cd macosx/
-ln -s dang.1.8.020529 dang
 ln -s gawk-3.1.3 gawk
 ln -s gawk-3.1.3 awk
-ln -s prekin.6.36.040609.macOSX prekin
-ln -s probe.2.9.030123.macosx probe
-ln -s reduce.2.21.mod040509dcr.macOSX reduce
 cd ..
 cd linux/
-ln -s prekin.6.36.040609.linux.i386.RH90.static prekin
-ln -s reduce.2.21.mod040509dcr.linux.RH9 reduce
+# Nothing to do here...
 cd ..
 cd ..
