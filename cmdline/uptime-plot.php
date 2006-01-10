b7519962eccb36fb7a78ba2c67a0ed466ba48e18
iwd <iwd@5035da28-9fce-0310-abbb-aec50abfc0e8>
2006-01-10 19:41:09 +0000

    - added ability to set/change resolution while editing a PDB file - tried yet another layout for the upload boxes
    git-svn-id: file:///home/smlewis/svn/git-mp3/another_svn/tmp/tmp/trunk/molprobity3@169 5035da28-9fce-0310-abbb-aec50abfc0e8

--- cmdline/uptime-plot.php
+++ cmdline/uptime-plot.php
@@ -2,9 +2,12 @@
 #
 # This script analyzes the output of a command like
 #
-#   (while true; do echo -n `date` '::' >> uptime.log; uptime >> uptime.log; sleep 60; done) &
+#   (while true; do echo -n `date +'%Y-%m-%d %H:%M:%S %z'` '::' >> uptime.log; uptime >> uptime.log; sleep 60; done) &
 #
 # given on stdin and writes a kinemage plot to stdout.
+#
+# The funny args to date() are because the PHP strtotime() doesn't like
+# the default date format, which is stupid but true.
 #
 $lines = file("php://stdin");
 $timestamps = array();
@@ -14,25 +17,27 @@ $sysload_15 = array();
 
 foreach($lines as $line)
 {
-    if(preg_match('/^\s*(\d+:\d+).+load average:\s+(\d+\.\d+),?\s+(\d+\.\d+),?\s+(\d+\.\d+)/', $line, $f))
+    if(preg_match('/^\s*(.+?)\s*::.*load average:?\s+(\d+\.\d+),?\s+(\d+\.\d+),?\s+(\d+\.\d+)/', $line, $f))
     {
-        #echo "time $f[1]    load $f[2]\n";
-        $timestamps[]   = $f[1];
+        //echo "time $f[1]    load $f[2]\n";
+        $timestamps[]   = strtotime($f[1]);
         $sysload_01[]   = $f[2];
         $sysload_05[]   = $f[3];
         $sysload_15[]   = $f[4];
     }
 }
 
-$scale = 3.0;
+$scale = 60*60*24; // 1 day
+//$scale = 60*60; // 1 hour
+$axis = (max($timestamps) - min($timestamps)) / $scale;
 
 echo "@kinemage 1
 @flatland
 @vectorlist {axes} color= gray
-{load} $scale 1 0
+{load} $axis 1 0
 {load} 0 1 0
 {origin} 0 0 0
-{time} $scale 0 0
+{time} $axis 0 0
 ";
 
 echo "@vectorlist {1 min. load} color= blue\n";
@@ -46,9 +51,13 @@ function printLoad($timestamps, $sysload)
 {
     global $scale;
     $len = count($timestamps);
+    $minT = min($timestamps);
+    //$maxT = max($timestamps);
+    //$lenT = $maxT - $minT;
     for($i = 0; $i < $len; $i++)
     {
-        echo "{".$timestamps[$i]."} ".($scale * $i / $len)." ".$sysload[$i]." 0\n";
+        $t = $timestamps[$i];
+        echo "{".date("D j M Y g:ia T", $t)."    ".$sysload[$i]."} ".(($t-$minT)/$scale)." ".$sysload[$i]." 0\n";
     }
 }
 ?>
--- pages/editpdb_setup2.php
+++ pages/editpdb_setup2.php
@@ -24,6 +24,13 @@ function display($context)
     echo "<h3>Select editing operations to perform:</h3>";
     echo "<div class='indent'>\n";
     
+    echo "<h5 class='nospaceafter'>Set parameters:</h5>";
+    echo "<div class='indent'>\n";
+    echo "<table border='0'>\n";
+    echo "<tr><td>Resolution:</td><td><input type='text' name='resolution' value='".($_SESSION['models'][$modelID]['stats']['resolution']+0)."' size='4'> &Aring;</td></tr>\n";
+    echo "</table>\n";
+    echo "</div>\n"; // end indent
+    
     echo "<h5 class='nospaceafter'>Remove unwanted chains:</h5>";
     echo "<div class='indent'>\n";
     foreach($_SESSION['models'][$modelID]['stats']['chainids'] as $chainID)
@@ -61,42 +68,54 @@ function onEditPDB($arg, $req)
     }
     
     // Otherwise, moving forward:
+    $oldID = $req['modelID'];
+    $oldModel = $_SESSION['models'][$oldID];
+    $inpath = $_SESSION['dataDir'].'/'.MP_DIR_MODELS.'/'.$oldModel['pdb'];
+    
+    $newModel = createModel($oldID."_edit");
+    $newID = $newModel['id'];
+    $outpath = $_SESSION['dataDir'].'/'.MP_DIR_MODELS.'/'.$newModel['pdb'];
+
+    $s = "";
     if(is_array($req['removechain']) && count($req['removechain']) > 0)
     {
-        $oldID = $req['modelID'];
-        $oldModel = $_SESSION['models'][$oldID];
-        $inpath = $_SESSION['dataDir'].'/'.MP_DIR_MODELS.'/'.$oldModel['pdb'];
-        
-        $newModel = createModel($oldID."_edit");
-        $newID = $newModel['id'];
-        $outpath = $_SESSION['dataDir'].'/'.MP_DIR_MODELS.'/'.$newModel['pdb'];
-    
         removeChains($inpath, $outpath, $req['removechain']);
-        $newModel['stats']      = pdbstat($outpath);
-        $newModel['history']    = "Edited $oldModel[pdb] to remove chains";
-        $_SESSION['models'][$newID] = $newModel;
-        $_SESSION['lastUsedModelID'] = $newID; // this is now the current model
-        
-        $s .= "You created $newModel[pdb] by removing chain(s) ".implode(', ', $req['removechain'])." from $oldModel[pdb].\n";
-        $details = describePdbStats($newModel['stats'], true);
-        $s .= "<ul>\n";
-        foreach($details as $detail) $s .= "<li>$detail</li>\n";
-        $s .= "</ul>\n";
-        
-        $entrynum = addLabbookEntry(
-            "Created PDB file $newModel[pdb]",
-            $s,
-            "$oldID|$newID",
-            "auto",
-            "scissors.png"
-        );
-        
+        $s .= "<p>You created $newModel[pdb] by removing chain(s) ".implode(', ', $req['removechain'])." from $oldModel[pdb].\n";
         mpLog("editpdb:Removed chains from a PDB file");
-        pageGoto("generic_done.php", array('labbookEntry' => $entrynum));
     }
+    else copy($inpath, $outpath);
+    
+    $resolu = $req['resolution']+0;
+    $oldRes = $oldModel['stats']['resolution']+0;
+    if($resolu && ($oldRes == 0 || $oldRes != $resolu) )
+    {
+        $remark2 = sprintf("REMARK   2                                                                      \nREMARK   2 RESOLUTION. %.2f ANGSTROMS.                                          \n", $resolu);
+        replacePdbRemark($outpath, $remark2, 2);
+        $s .= "<p>You manually set the resolution for $newModel[pdb].\n";
+        mpLog("editpdb:Changed/set resolution for a PDB file");
+    }
+
+    $newModel['stats']      = pdbstat($outpath);
+    $newModel['history']    = "Edited $oldModel[pdb]";
+    $_SESSION['models'][$newID] = $newModel;
+    $_SESSION['lastUsedModelID'] = $newID; // this is now the current model
+    
+    $details = describePdbStats($newModel['stats'], true);
+    $s .= "<ul>\n";
+    foreach($details as $detail) $s .= "<li>$detail</li>\n";
+    $s .= "</ul>\n";
+    
+    $entrynum = addLabbookEntry(
+        "Created PDB file $newModel[pdb]",
+        $s,
+        "$oldID|$newID",
+        "auto",
+        "scissors.png"
+    );
+    
+    pageGoto("generic_done.php", array('labbookEntry' => $entrynum));
 }
 #}}}########################################################################
-
 
 #{{{ a_function_definition - sumary_statement_goes_here
 ############################################################################
