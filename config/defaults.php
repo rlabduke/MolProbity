d751aabe01d688b739e2abe015b251094f7cdbb3
iwd <iwd@5035da28-9fce-0310-abbb-aec50abfc0e8>
2006-03-24 20:35:43 +0000

    - added Life, the Universe, and Everything question to april_fools.php - updated Linux prekin to stop seg fault on long filenames - increased -Max# for cluster within clashlist, to fix bug running 1iw7
    git-svn-id: file:///home/smlewis/svn/git-mp3/another_svn/tmp/tmp/trunk/molprobity3@178 5035da28-9fce-0310-abbb-aec50abfc0e8

--- config/defaults.php
+++ config/defaults.php
@@ -114,6 +114,6 @@ if(!defined("MP_DIR_CHARTS"))
     define("MP_SESSION_NAME", "MolProbSID");
 
 // Current "internal reference" version number. Please DO NOT change.
-    define("MP_VERSION", "3beta28");
+    define("MP_VERSION", "3beta29");
 
 ?>
