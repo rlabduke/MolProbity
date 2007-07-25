6f8929dc6f0a7b88a452c55eaa196037ae7f862a
vbc3 <vbc3@5035da28-9fce-0310-abbb-aec50abfc0e8>
2007-07-25 23:02:45 +0000

    - updated references to new NAR 2007 reference. - added warning about PDBv3 files.
    git-svn-id: file:///home/smlewis/svn/git-mp3/another_svn/tmp/tmp/branches/pdbv3temp@256 5035da28-9fce-0310-abbb-aec50abfc0e8

--- pages/welcome.php
+++ pages/welcome.php
@@ -78,15 +78,10 @@ from your web browser.</p>
 
 <h3>Common questions:</h3>
 <p><b><a href='help/about.html' target='_blank'>Cite MolProbity</a></b>:
-    <small>Lovell et al. (2003)
-    <a href="http://kinemage.biochem.duke.edu/validation/valid.html" target="_blank">Structure
-    validation by C-alpha geometry: phi, psi, and C-beta deviation.</a>
-    Proteins 50:437-450
-    <b>or</b>
-    Davis et al. (2004)
+    <small>Davis et al. (2007)
     <a href="http://kinemage.biochem.duke.edu/lab/papers.php" target="_blank">MolProbity:
-    structure validation and all-atom contact analysis for nucleic acids and their complexes.</a>
-    Nucleic Acids Research 32:W615-W619.
+    all-atom contacts and structure validation for proteins and nucleic acids.</a>
+    Nucleic Acids Research 35:W375-W383.
     </small></p>
 <p><b><a href='help/java.html' target='_blank'>Installing Java</a></b>: how to make kinemage graphics work in your browser.</p>
 <p><b><a href='get_molprobity.php' target='_blank'>Download MolProbity</a></b>: how can I run a private MolProbity server, or run from the command line?</p>
@@ -600,6 +595,10 @@ function toggleUploadOptions()
     </div>
 </div></form>
 <?php
+
+    echo("<div class=alert><strong>The PDB is currently changing its file format (see <a href='http://remediation.wwpdb.org/index.html' target='_blank'>here</a> for more info).  
+        We are currently upgrading our software to be compatible with the new format.
+        <br>Please don't hesitate to report any bugs you may encounter; sorry for any inconvenience.</strong></div>");
 }
 #}}}########################################################################
 
