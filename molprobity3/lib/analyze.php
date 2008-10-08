e93584e6704dbe5b1595119cfee014dd829ce333
vbc3 <vbc3@5035da28-9fce-0310-abbb-aec50abfc0e8>
2008-10-08 21:33:13 +0000

    New separated dangle jar from chiropraxis. I guess I forgot to add remark42 to svn.
    git-svn-id: file:///home/smlewis/svn/git-mp3/another_svn/tmp/tmp/trunk@324 5035da28-9fce-0310-abbb-aec50abfc0e8

--- molprobity3/lib/analyze.php
+++ molprobity3/lib/analyze.php
@@ -879,7 +879,7 @@ function findRamaOutliers($rama)
 ############################################################################
 function runSuitenameReport($infile, $outfile)
 {
-    exec("java -Xmx256m -cp ".MP_BASE_DIR."/lib/chiropraxis.jar chiropraxis.dangle.Dangle rnabb $infile | suitename -report > $outfile");
+    exec("java -Xmx256m -cp ".MP_BASE_DIR."/lib/dangle.jar dangle.Dangle rnabb $infile | suitename -report > $outfile");
 }
 #}}}########################################################################
 
@@ -966,7 +966,7 @@ function runSuitenameString($infile, $outfile)
     // Unix "fold" is used to wrap long lines to reasonable lengths,
     // so they display OK in the <PRE> region of the HTML page.
     // 60 was selected because it makes counting to specific positions easier (20 suites/line)
-    exec("java -Xmx256m -cp ".MP_BASE_DIR."/lib/chiropraxis.jar chiropraxis.dangle.Dangle rnabb $infile | suitename -string -oneline | fold -w 60 > $outfile");
+    exec("java -Xmx256m -cp ".MP_BASE_DIR."/lib/dangle.jar dangle.Dangle rnabb $infile | suitename -string -oneline | fold -w 60 > $outfile");
 }
 #}}}########################################################################
 
@@ -975,7 +975,7 @@ function runSuitenameString($infile, $outfile)
 ############################################################################
 function runValidationReport($infile, $outfile, $moltype)
 {
-    exec("java -Xmx256m -cp ".MP_BASE_DIR."/lib/chiropraxis.jar chiropraxis.dangle.Dangle -$moltype -validate -outliers -sigma=0.0 $infile > $outfile");
+    exec("java -Xmx256m -cp ".MP_BASE_DIR."/lib/dangle.jar dangle.Dangle -$moltype -validate -outliers -sigma=0.0 $infile > $outfile");
 }
 #}}}########################################################################
 
