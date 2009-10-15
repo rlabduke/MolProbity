f9b6989afbf74917c6e8a236656e2c59a8642bdb
vbc3 <vbc3@5035da28-9fce-0310-abbb-aec50abfc0e8>
2009-10-15 19:56:36 +0000

    Changed welcome page to reflect new 3.17. Updated About page.
    git-svn-id: file:///home/smlewis/svn/git-mp3/another_svn/tmp/tmp/trunk@353 5035da28-9fce-0310-abbb-aec50abfc0e8

--- molprobity3/config/defaults.php
+++ molprobity3/config/defaults.php
@@ -133,6 +133,6 @@ if(!defined("MP_DIR_CHARTS"))
     define("MP_SESSION_NAME", "MolProbSID");
 
 // Current "internal reference" version number. Please DO NOT change.
-    define("MP_VERSION", "3.16");
+    define("MP_VERSION", "3.17");
 
 ?>
