bc9e8d0dd5cfce222e4146a60e3f9f5367f6d02a
vbc3 <vbc3@5035da28-9fce-0310-abbb-aec50abfc0e8>
2013-06-20 22:37:38 +0000

    make_condor_files now creates all needed .sub files
    git-svn-id: file:///home/smlewis/svn/git-mp3/another_svn/tmp/tmp/trunk@477 5035da28-9fce-0310-abbb-aec50abfc0e8

--- molprobity3/cmdline/make_condor_files.py
+++ molprobity3/cmdline/make_condor_files.py
@@ -2,14 +2,17 @@
 # (jEdit options) :folding=explicit:collapseFolds=1:
 import sys, os, getopt, re, subprocess
 
+# THIS FILE MUST BE IN THE MOLPROBITY CMDLINE DIRECTORY!!!
+
 #number_to_run = 50
+total_file_size_limit = 10000000 # 10 MB by default
 
 #{{{ parse_cmdline
 #parse the command line--------------------------------------------------------------------------
 def parse_cmdline():
   try:
     #opts, args = getopt.getopt(sys.argv[1:], 'hn:',['help', 'number='])
-    opts, args = getopt.getopt(sys.argv[1:], 'h',['help'])
+    opts, args = getopt.getopt(sys.argv[1:], 'hl',['help', 'limit='])
   except getopt.GetoptError as err:
     print str(err)
     help()
@@ -18,16 +21,21 @@ def parse_cmdline():
     if o in ("-h", "--help"):
       help()
       sys.exit()
-    #elif o in ("-n", "--number"):
-    #  number_to_run = a
+    elif o in ("-l", "--limit"):
+      if a > 1000000: # no less than 1 MB
+        global total_file_size_limit
+        total_file_size_limit = a
+      else:
+        sys.stderr.write("\n**ERROR: -limit cannot be less than 1000000\n")
+        sys.exit(help())
   return args
-  if len(args) < 2:
-    sys.stderr.write("\n**ERROR: User must specify output directory and input directory\n")
+  if len(args) < 1:
+    sys.stderr.write("\n**ERROR: User must specify input directory\n")
     sys.exit(help())
   else:
-    outdir = args[0]
-    if (os.path.isdir(outdir)):
-      return outdir, args[1:]
+    indir = args[0]
+    if (os.path.isdir(indir)):
+      return indir
     else:
       sys.stderr.write("\n**ERROR: First argument must be a directory!\n")
       sys.exit(help())
@@ -36,21 +44,18 @@ def parse_cmdline():
 
 #{{{ help
 def help():
-  print """USAGE:   python preprocess_pdb.py [output_directory] [pdb_file or directory]
+  print """USAGE:   python make_condor_files.py [input_directory_of_pdbs]
   
-  [output_directory]      Directory for all output files to go
-  [pdb_file or directory] Takes as input either pdb_files or a directory of files.
-                          Note that this script will convert ALL files in the directory.
-                          
-  This script adds REMARK tags to any lines that aren't ATOM, TER, or END.
-  It remediates files, since some files come with hydrogens.
-  It also checks atom names to make sure they are justified correctly.
+  Takes as input a directory containing pdbs, and generates a directory 'condor_sub_files' 
+  within that directory containing all the scripts needed to run molprobity analysis on a 
+  HTCondor cluster.
   
 FLAGS:
   -h     Print this help message
 """
 #}}}
 
+#{{{ split_pdbs
 def split_pdbs(in_dir, size_limit):
   if (os.path.isdir(in_dir)):
     files = os.listdir(in_dir)
@@ -63,36 +68,42 @@ def split_pdbs(in_dir, size_limit):
     #print files
     for f in files:
       #arg_file = os.path.join(arg, f)
-      if (not os.path.isdir(os.path.realpath(f))):
+      full_file = os.path.realpath(f)
+      if (not os.path.isdir(full_file)):
         root, ext = os.path.splitext(f)
         if (ext == ".pdb"):
           #print f
           if (list_size <= size_limit):
-            pdb_list.append(f)
+            pdb_list.append(full_file)
             #print pdb_list
-            list_size = list_size + os.path.getsize(f)
+            list_size = list_size + os.path.getsize(full_file)
           else:
             pdb_list = []
-            pdb_list.append(f)
+            pdb_list.append(full_file)
             list_of_lists.append(pdb_list)
-            list_size = os.path.getsize(f)
+            list_size = os.path.getsize(full_file)
+    if len(list_of_lists) > 500:
+      sys.stderr.write("\n**ERROR: More than 500 jobs needed, try choosing a larger -limit\n")
+      sys.exit(help())
     return list_of_lists
     #print list_of_lists
+#}}}
 
 #{{{ write_super_dag
 def write_super_dag(outdir, list_of_pdblists):
   out_name = "supermol.dag"
   outfile = os.path.join(os.path.realpath(outdir), out_name)
   out=open(outfile, 'wr')
-  for num, pdbs in enumerate(list_of_pdblists):
-    out.write("SUBDAG EXTERNAL "+repr(num)+" moldag"+repr(num)+".dag\n")
+  for indx, pdbs in enumerate(list_of_pdblists):
+    num = '{:0>3}'.format(indx)
+    out.write("SUBDAG EXTERNAL "+num+" moldag"+num+".dag\n")
     write_mol_dag(outdir, num, pdbs)
   out.close()
 #}}}
 
 #{{{ write_mol_dag
 def write_mol_dag(outdir, num, pdbs):
-  out_name = "moldag"+repr(num)+".dag"
+  out_name = "moldag"+num+".dag"
   outfile = os.path.join(os.path.realpath(outdir), out_name)
   out=open(outfile, 'wr')
   
@@ -102,15 +113,15 @@ def write_mol_dag(outdir, num, pdbs):
   
   parent_childs = "" # create parent_childs part of dag file now so only have to loop once thru pdbs
   for pdb_file in pdbs:
-    pdb, ext = os.path.splitext(pdb_file)
+    pdb, ext = os.path.splitext(os.path.basename(pdb_file))
     out.write("Job clash"+pdb+" clashlist.sub\n")
     out.write("VARS clash"+pdb+" PDB=\""+pdb_file+"\"\n")
     out.write("\n")
-    parent_childs = parent_childs+"PARENT clash"+pdb+" CHILD local"+repr(num)+"\n"
+    parent_childs = parent_childs+"PARENT clash"+pdb+" CHILD local"+num+"\n"
   
-  out.write("Job local"+repr(num)+" local.sub\n")
-  out.write("VARS local"+repr(num)+" PDB=\""+" ".join(pdbs)+"\"\n")
-  out.write("VARS local"+repr(num)+" NUMBER=\""+repr(num)+"\"\n")
+  out.write("Job local"+num+" local.sub\n")
+  out.write("VARS local"+num+" PDB=\""+" ".join(pdbs)+"\"\n")
+  out.write("VARS local"+num+" NUMBER=\""+num+"\"\n")
   out.write(parent_childs)
   
   out.close()
@@ -131,11 +142,107 @@ def run_clashlist(outdir, arg):
   err.close()
 #}}}
 
+#{{{ write_file
+def write_file(outdir, out_name, file_text):
+  outfile = os.path.join(os.path.realpath(outdir), out_name)
+  out=open(outfile, 'wr')
+  out.write(file_text)
+  out.close()
+#}}}
+
+#{{{ write_local_run
+local_run = """#!/bin/sh
+
+# Get the pdb file location from args
+#pdb=$1
+
+for pdb in "$@"
+do
+# Chiropraxis
+java -cp {0}/lib/chiropraxis.jar chiropraxis.rotarama.Ramalyze -raw $pdb > results/${pdb}-ramalyze
+
+# Rotalyze
+java -cp {0}/lib/chiropraxis.jar chiropraxis.rotarama.Rotalyze $pdb > results/${pdb}-rotalyze
+
+# Prekin -pperp
+{0}/bin/linux/prekin -pperptoline -pperpdump $pdb > results/${pdb}-prekin_pperp
+
+# Dangle rna
+java -cp {0}/lib/dangle.jar dangle.Dangle -rna -validate -outliers -sigma=0.0 $pdb > results/${pdb}-dangle_rna
+
+# c-beta dev
+{0}/bin/linux/prekin -cbdevdump $pdb > results/${pdb}-cbdev
+
+# Dangle protein
+java -cp {0}/lib/dangle.jar dangle.Dangle -protein -validate -outliers -sigma=0.0 $pdb > results/${pdb}-dangle_protein
+
+# Suitename
+java -Xmx512m -cp {0}/lib/dangle.jar dangle.Dangle rnabb $pdb | {0}/bin/linux/suitename  -report > results/${pdb}-suitename
+
+# Analyze the results
+{0}/cmdline/molparser.py -q $pdb 1 results/${pdb}-clashlist results/${pdb}-cbdev results/${pdb}-rotalyze results/${pdb}-ramalyze results/${pdb}-dangle_protein results/${pdb}-dangle_rna results/${pdb}-prekin_pperp results/${pdb}-suitename
+done
+"""
+#}}}
+
+#{{{ write_localsub
+local_sub = """universe = local
+
+Notify_user  = vbchen@bmrb.wisc.edu
+notification = Error
+
+Executable	= local_run.sh
+Arguments	=  $(PDB)
+
+log		= logs/local$(NUMBER).log
+output		= logs/local$(NUMBER).out
+error		= logs/local$(NUMBER).err
+copy_to_spool	= False
+priority	= 0
+
+queue
+"""
+#}}}
+
+#{{{ write_clashlistsub
+clash_sub = """universe = vanilla
+
+Notify_user  = vbchen@bmrb.wisc.edu
+notification = Error
+
+requirements = ((TARGET.FileSystemDomain == "bmrb.wisc.edu") || (TARGET.FileSystemDomain == ".bmrb.wisc.edu"))
+
+Executable	= {0}/bin/clashlist
+Arguments	= $(PDB) 40 10
+
+should_transfer_files = YES
+when_to_transfer_output = ON_EXIT
+transfer_input_files = $(PDB),{0}/bin/linux/probe,{0}/bin/linux/cluster
+
+log		= logs/clashlist.log
+output		= results/$(PDB)-clashlist
+error		= logs/clashlist.err
+copy_to_spool	= False
+priority	= 0
+
+queue
+"""
+#}}}
+
 if __name__ == "__main__":
-  outdir, arg = parse_cmdline()
-  print arg
-  if os.path.exists(arg):
-    list_of_lists = split_pdbs(arg, 5000000)
+  molprobity_home = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+  indir = parse_cmdline()[0]
+  if os.path.exists(indir):
+    outdir = os.path.join(indir, "condor_sub_files")
+    if not os.path.exists(outdir):
+      os.makedirs(outdir)
+    else:
+      sys.stderr.write("\"condor_sub_files\" directory detected in \""+indir+"\", please delete it before running this script\n")
+      sys.exit()
+    list_of_lists = split_pdbs(indir, total_file_size_limit)
     write_super_dag(outdir, list_of_lists)
+    write_file(outdir, "local_run.sh", local_run.format(molprobity_home, pdb="{pdb}"))
+    write_file(outdir, "local.sub", local_sub)
+    write_file(outdir, "clashlist.sub", clash_sub.format(molprobity_home))
   else:
-    print arg + " does not seem to exist!"
+    sys.stderr.write(indir + " does not seem to exist!\n")
