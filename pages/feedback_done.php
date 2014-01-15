b1ad1753ce94f886109640635598d0d1ee5fea00
jheadd <jheadd@5035da28-9fce-0310-abbb-aec50abfc0e8>
2014-01-15 21:15:42 +0000

    clarified txt asking users to not delete their session after reporting a bug
    git-svn-id: file:///home/smlewis/svn/git-mp3/another_svn/tmp/tmp/branches/moltbx@537 5035da28-9fce-0310-abbb-aec50abfc0e8

--- pages/feedback_done.php
+++ pages/feedback_done.php
@@ -4,7 +4,7 @@
 *****************************************************************************/
 // We use a uniquely named wrapper class to avoid re-defining display(), etc.
 class feedback_done_delegate extends BasicDelegate {
-    
+
 #{{{ display - creates the UI for this page
 ############################################################################
 /**
@@ -16,10 +16,10 @@ function display($context)
     echo "<p>Your email was successfully sent to the author(s) and maintainer(s) of MolProbity.\n";
     echo "You should receive a response, if needed, within a few days.</p>\n";
 
-    echo "<p><div class='alert'>If you're having a problem or think you've found a bug, and\n";
-    echo "if you don't mind the MolProbity maintainers seeing your data files,\n";
+    echo "<p><div class='alert'>If you're having a problem or think you've found a bug, <strong>please do not delete the session </strong> associated with this report.  We need the input files to reproduce the bug, and we will take care of deleting those files later.  Your files will only be used for debugging purposes.  <br><br>We will contact you for the input files if you delete the session accidentally (assuming you provided your email).</div></p>\n";
+    /*echo "if you don't mind the MolProbity maintainers seeing your data files,\n";
     echo "then please do NOT log out and erase your files.\n";
-    echo "This will help us diagnose and correct the problem.</div></p>\n";
+    echo "This will help us diagnose and correct the problem.</div></p>\n";*/
 
     echo "<hr><tt>\n";
     echo nl2br(htmlspecialchars($context));
