7b74ae7c2227ee6f083a80d7737599a68a636d28
Merlin Hartley <merlinhartley@hotmail.com>
2015-08-24 14:53:44 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -32,7 +32,7 @@ if [ ! -f scons.tar.gz ]; then curl http://kinemage.biochem.duke.edu/molprobity/
 if [ ! -f annlib.tar.gz ]; then curl http://kinemage.biochem.duke.edu/molprobity/annlib.tar.gz -o annlib.tar.gz; fi
 if [ ! -f annlib_adaptbx.tar.gz ]; then curl http://kinemage.biochem.duke.edu/molprobity/annlib_adaptbx.tar.gz -o annlib_adaptbx.tar.gz; fi
 #if [ ! -f cbflib.tar.gz ]; then curl http://kinemage.biochem.duke.edu/molprobity/cbflib.tar.gz -o cbflib.tar.gz; fi
-if [ ! -f cbflib.tar.gz ]; then curl http://sourceforge.net/projects/cbflib/files/cbflib/CBFlib_0.9.5/CBFlib-0.9.5.11.tar.gz/download -o cbflib.tar.gz; fi
+if [ ! -f cbflib.tar.gz ]; then curl http://downloads.sourceforge.net/project/cbflib/cbflib/CBFlib_0.9.5/CBFlib-0.9.5.11.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fcbflib%2Ffiles%2Fcbflib%2FCBFlib_0.9.5%2F&ts=1440424361&use_mirror=netcologne -o cbflib.tar.gz; fi
 if [ ! -f ccp4io.tar.gz ]; then curl http://kinemage.biochem.duke.edu/molprobity/ccp4io.tar.gz -o ccp4io.tar.gz; fi
 if [ ! -f ccp4io_adaptbx.tar.gz ]; then curl http://kinemage.biochem.duke.edu/molprobity/ccp4io_adaptbx.tar.gz -o ccp4io_adaptbx.tar.gz; fi
 if [ ! -f chem_data.tar.gz ]; then curl http://kinemage.biochem.duke.edu/molprobity/chem_data.tar.gz -o chem_data.tar.gz; fi
