b8b46e9d174dce2fd05010995793c389dfa4de57
Steven Lewis <smlewi@gmail.com>
2015-11-11 11:41:13 -0500

    tweaking the test script: ready to test new generation

--- test/test_script_wrapper.sh
+++ test/test_script_wrapper.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
-#this script runs a regression test suite for MolProbity's dependencies
+#This script runs a test suite for MolProbity's dependencies across a small number of PDBs.
+#It's meant to excercise most of the major functions.
+
+#The test suite:
+
+# 1A2P.pdb   Protein, no flips
+# 4NPD.pdb   Protein, has many alternates
+# 1UBQ.pdb   Protein, small, OK, ubiquitous
+# 2V8O.pdb   Protein, exercises omegalyze
+# 3KAT.pdb   Protein, small, low-res
+# 4HUM.pdb   Protein, small, many errors, exercises CaBLAM
+
+# 1VC7.pdb   RNA with many errors
+# 4PRF.pdb   (same) RNA with very few errors
+# 1EHZ.pdb   RNA, moderate number of known errors
+
+#ISSUES: no NMR structure to test nuclear hydrogen bond lengths, no CDL structure
+
+
 (#parens scopes variables
 
 
@@ -8,17 +26,20 @@ $home = pwd
 
 mkdir $home/new
 
-#pseudocode
-for each in (1UBQ, ???, ???): #may need to unloop for simple_molprobity's flags, and function-ize some of the loop
-cd $home/new
-simple_molprobity.sh > log.$each # redirect version that captures all output
-mv log.$each $home/new/test_$each
+#may need to unloop for simple_molprobity's flags, and function-ize some of the loop
+for each in ("1A2P.pdb", "1EHZ.pdb", "1UBQ.pdb", "1VC7.pdb", "2V8O.pdb", "3KAT.pdb", "4HUM.pdb", "4NPD.pdb", "4PRF.pdb")
+do
+    echo "Procesing $each"
+    cd $home/new
+    $home/simple_molprobity.sh $home/pdbtestfiles/$each > log.$each # redirect version that captures all output
+    mv log.$each $home/new/test_$each
 
 #maybe should re-loop to do work-then-compare-later?
-if ref directory exists:
-diff --brief $home/ref/test_$each $home/new/test_$each
+#if ref directory exists:
+#diff --brief $home/ref/test_$each $home/new/test_$each
 
-else:
-mv new ref
+#else:
+#mv new ref
+done
 
 ) #Trailing parenthesis to limit scope of script vars
