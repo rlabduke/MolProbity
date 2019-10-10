c24fdd39ea22ab38c9c23e8fd523263e34cfba04
chrissciwilliams <christopher.sci.williams@gmail.com>
2019-10-10 18:04:46 -0400

    several changes to display of chiral volume validation

--- cmdline/parse_chiral_report.py
+++ cmdline/parse_chiral_report.py
@@ -3,6 +3,10 @@ from iotbx.pdb.hybrid_36 import hy36decode
 
 #This script parses the output of mmtbx.mpgeo to find results for chiral volume validation
 #It assembles those results into a text file of outliers
+#Three categories:
+#1. True chiral centers with handedness swaps
+#2. Tetrahedral centers with bad geometry
+#3. Pseudochiral centers that look like handedness swap bc of atom naming errors
 
 class chiral_volume():
   def __init__(self, line):
@@ -14,14 +18,36 @@ class chiral_volume():
     self.resname = x[5]
     self.atom = x[6]
     self.chiral_volume = x[7]
-    self.sigma = x[8]
+    self.sigma = float(x[8])
     #cutoff for identification of outliers is 4 sigma
-    if float(self.sigma) > 4:
+    if self.sigma > 4:
       self.is_outlier = True
     else:
       self.is_outlier = False
+    if self.sigma > 10:
+      self.is_handedness_swap = True
+    else:
+      self.is_handedness_swap = False
     self.moltype = x[9].rstrip()
     self.resid = ":".join([self.chain, self.resseq+self.icode, self.altloc, self.resname, self.atom])
+
+  def is_pseudochiral(self):
+    #Certain atoms are treated like chiral centers because they bond to atoms that have different names without chemical difference.
+    #VAL CB bonds to CG1 and CG2, for example.
+    #A large chiral volume outlier relfects a failure to follow chemical naming conventions, not necessarily a major geometry error
+    #So these pseudochiral centers should be treated differently.
+    #
+    #backbone phosphate in nucleic acids
+    #OP1 and OP2 atoms are chemically identical
+    if self.atom == 'P': return True
+    #SF4 and F3S are iron-sulfur clusters with frequent naming problems
+    if self.resname in ['SF4','F3S']: return True
+    #Val CG1 and CG2 are chemically identical
+    if self.resname == 'VAL' and self.atom == 'CB': return True
+    #LEU CD1 and CD2 are chemically identical
+    if self.resname == 'LEU' and self.atom == 'CG': return True
+    #Otherwise
+    return False
 
   def make_recommendation(self):
     if float(self.sigma) > 10:
@@ -30,14 +56,16 @@ class chiral_volume():
       self.recommendation = ""
 
   def make_output_line(self):
-    return ":".join([self.resid, self.sigma, self.recommendation])
+    return "%s:%.2f" %(self.resid, self.sigma)
+#    return ":".join([self.resid, self.sigma, self.recommendation])
 
 geomfile = open(sys.argv[1])
 
-backbone_chiral_center_count = 0
-other_chiral_center_count = 0
-backbone_ca_outliers = []
-other_outliers = []
+handedness_outliers = []
+tetrahedral_outliers = []
+pseudochiral_naming_errors = []
+all_tetrahedral_center_count = 0
+true_chiral_center_count = 0
 
 for line in geomfile:
 #6O4M_messed.pdb: B: 103: : :SO4:O3-S-O4:109.930:0.153:PROTEIN
@@ -50,45 +78,79 @@ for line in geomfile:
   if '-' in atom:
     continue
   chiral = chiral_volume(line)
-  if chiral.atom == 'CA':
-    backbone_chiral_center_count += 1
-  else:
-    other_chiral_center_count += 1
+
+  all_tetrahedral_center_count += 1
+  if not chiral.is_pseudochiral():
+    true_chiral_center_count += 1
+
   if not chiral.is_outlier:
     continue
-  chiral.make_recommendation()
-  if chiral.atom == 'CA':
-    backbone_ca_outliers.append(chiral)
+
+  if chiral.is_handedness_swap:
+    if chiral.is_pseudochiral():
+      pseudochiral_naming_errors.append(chiral)
+    else:
+      handedness_outliers.append(chiral)
   else:
-    other_outliers.append(chiral)
+    tetrahedral_outliers.append(chiral)
 
-backbone_ca_outliers.sort(key=lambda r: (r.chain, int(hy36decode(len(r.resseq), r.resseq)), r.icode, r.altloc))
-other_outliers.sort(key=lambda r: (r.chain, int(hy36decode(len(r.resseq), r.resseq)), r.icode, r.altloc))
+handedness_outliers.sort(key=lambda r: (r.chain, int(hy36decode(len(r.resseq), r.resseq)), r.icode, r.altloc))
+tetrahedral_outliers.sort(key=lambda r: (r.chain, int(hy36decode(len(r.resseq), r.resseq)), r.icode, r.altloc))
+pseudochiral_naming_errors.sort(key=lambda r: (r.chain, int(hy36decode(len(r.resseq), r.resseq)), r.icode, r.altloc))
 
+total_outliers = len(handedness_outliers)+len(tetrahedral_outliers)+len(pseudochiral_naming_errors)
 
-if backbone_chiral_center_count == 0:
-  percent_backbone_outliers = 0
+if true_chiral_center_count == 0:
+  percent_chiral_outliers = 0
 else:
-  percent_backbone_outliers = len(backbone_ca_outliers)/backbone_chiral_center_count*100
+  percent_chiral_outliers = len(handedness_outliers)/true_chiral_center_count*100
 
-if other_chiral_center_count == 0:
-  percent_other_outliers = 0
+if all_tetrahedral_center_count == 0:
+  percent_total_outliers = 0
 else:
-  percent_other_outliers = len(other_outliers)/other_chiral_center_count*100
+  percent_total_outliers = total_outliers/all_tetrahedral_center_count*100
 
-sys.stdout.write("Chiral volume outliers around protein backbone CA\n")
-sys.stdout.write("SUMMARY: %i outliers out of %i CA chiral centers (%.2f%%)\n" % (len(backbone_ca_outliers), backbone_chiral_center_count, percent_backbone_outliers))
+sys.stdout.write("SUMMARY: %i total outliers at %i tetrahedral centers (%.2f%%)\n" % (total_outliers, all_tetrahedral_center_count, percent_total_outliers))
+sys.stdout.write("SUMMARY: %i handedness outliers at %i chiral centers (%.2f%%)\n" % (len(handedness_outliers), true_chiral_center_count, percent_chiral_outliers))
+sys.stdout.write("SUMMARY: %i tetrahedral geometry outliers\n" % (len(tetrahedral_outliers)))
+sys.stdout.write("SUMMARY: %i pseudochiral naming errors\n" % (len(pseudochiral_naming_errors)))
+
+sys.stdout.write("\nHandedness swaps\n")
 sys.stdout.write("----------------------------------------------------------------------\n")
-for chiral in backbone_ca_outliers:
+for chiral in handedness_outliers:
+  sys.stdout.write(chiral.make_output_line()+'\n')
+sys.stdout.write("----------------------------------------------------------------------\n\n")
+sys.stdout.write("Tetrahedral geometry outliers\n")
+sys.stdout.write("----------------------------------------------------------------------\n")
+for chiral in tetrahedral_outliers:
+  sys.stdout.write(chiral.make_output_line()+'\n')
+sys.stdout.write("----------------------------------------------------------------------\n\n")
+sys.stdout.write("Probable atom naming errors around pseudochiral centers\n")
+sys.stdout.write("  e.g. CG1 and CG2 around Valine CB\n")
+sys.stdout.write("----------------------------------------------------------------------\n")
+for chiral in pseudochiral_naming_errors:
   sys.stdout.write(chiral.make_output_line()+'\n')
 sys.stdout.write("----------------------------------------------------------------------\n")
-sys.stdout.write("\n")
-sys.stdout.write("Chiral volume outliers around other atoms\n")
-sys.stdout.write("SUMMARY: %i outliers out of %i other chiral centers (%.2f%%)\n" % (len(other_outliers), other_chiral_center_count, percent_other_outliers))
-sys.stdout.write("----------------------------------------------------------------------\n")
-for chiral in other_outliers:
-  sys.stdout.write(chiral.make_output_line()+'\n')
-sys.stdout.write("----------------------------------------------------------------------\n")
 
+#sys.stdout.write("Probable handedness swaps\n")
+#sys.stdout.write("SUMMARY: %i outliers out of %i CA chiral centers (%.2f%%)\n" % (len(backbone_ca_outliers), backbone_chiral_center_count, percent_backbone_outliers))
+#sys.stdout.write("----------------------------------------------------------------------\n")
+#for chiral in backbone_ca_outliers:
+#  sys.stdout.write(chiral.make_output_line()+'\n')
+#sys.stdout.write("----------------------------------------------------------------------\n")
+#sys.stdout.write("\n")
+#sys.stdout.write("Probable tetrahedral geometry outliers\n")
+#sys.stdout.write("SUMMARY: %i outliers out of %i other chiral centers (%.2f%%)\n" % (len(other_outliers), other_chiral_center_count, percent_other_outliers))
+#sys.stdout.write("----------------------------------------------------------------------\n")
+#for chiral in other_outliers:
+#  sys.stdout.write(chiral.make_output_line()+'\n')
+#sys.stdout.write("----------------------------------------------------------------------\n")
+#sys.stdout.write("\n")
+#sys.stdout.write("Probable atom naming errors around pseudochiral centers\n")
+#sys.stdout.write("SUMMARY: %i outliers out of %i other chiral centers (%.2f%%)\n" % (len(other_outliers), other_chiral_center_count, percent_other_outliers))
+#sys.stdout.write("----------------------------------------------------------------------\n")
+#for chiral in other_outliers:
+#  sys.stdout.write(chiral.make_output_line()+'\n')
+#sys.stdout.write("----------------------------------------------------------------------\n")
 
   
--- lib/visualize.php
+++ lib/visualize.php
@@ -1355,16 +1355,34 @@ function makeSummaryStatsTable($resolution, $clash, $rama, $rota, $cbdev, $pperp
     if(is_array($chiral_summary)) $addedRows +=1;
     if($addedRows > 0)
     {
-      $entry .= "<tr><td rowspan='$addedRows' align='center'>Additional validations</td>\n";
+      #$entry .= "<tr><td rowspan='$addedRows' align='center'>Additional validations</td>\n";
       $firstRow = true;
       if(is_array($chiral_summary))
       {
-        $chiral_outliers = $chiral_summary['outliers'];
-        $chiral_centers = $chiral_summary['centers'];
-        $chiralPct =  number_format ( $chiral_summary['percent_outliers'], $decimals = 2);
+        $chiral_outliers = $chiral_summary['chiral_outliers'];
+        $chiral_centers = $chiral_summary['chiral_centers'];
+        $chiralPct =  number_format ( $chiral_summary['percent_chiral_outliers'], $decimals = 2);
+        $tetra_outliers = $chiral_summary['tetra_outliers'];
+        $tetra_centers = $chiral_summary['tetra_centers'];
+        $total_outliers = $chiral_summary['total_outliers'];
+        $pseudo_outliers = $chiral_summary['pseudo_outliers'];
+        if ($total_outliers == 0)
+        {
+          $entry .= "<tr><td rowspan='$addedRows' align='center'>Additional validations</td>\n";
+          $entry .= "<td>Chiral volume outliers</td><td colspan=2>$total_outliers/$tetra_centers</td><td></td></tr>";
+        }
+        else
+        {
+          $addedRows +=2;
+          $entry .= "<tr><td rowspan='$addedRows' align='center'>Additional validations</td>\n";
+          $entry .= "<td>Chiral handedness swaps</td><td>$chiral_outliers/$chiral_centers</td><td>$chiralPct%</td><td>See Chiral volume report for details</td></tr>";
+          $entry .= "<td>Tetrahedral geometry outliers</td><td colspan=2>$tetra_outliers</td><td></td></tr>";
+          $entry .= "<td>Pseudochiral naming errors</td><td colspan=2>$pseudo_outliers</td><td></td></tr>";
+        }
         #if($chiral_centers == 0) $chiralPct = 0;
         #else $chiralPct = $chiral_summary['percent_outliers'];
-        $entry .= "<td>Chiral volume outliers</td><td>$chiral_outliers/$chiral_centers</td><td>$chiralPct%</td><td>See Chiral volume report for details</td></tr>\n";
+        #$entry .= "<td>Chiral handedness outliers</td><td>$chiral_outliers/$chiral_centers</td><td>$chiralPct%</td><td></td></tr>\n";
+        #$entry .= "<tr><td>Total chiral volume outliers</td><td>$tetra_outliers/$tetra_centers</td><td>$tetraPct%</td><td>See Chiral volume report for details</td></tr>\n";
       }
     }
         
